b4fe643aeaba50b55956b9d69ca94f7e60c035ee
donny2131 <55747762+donny2131@users.noreply.github.com>
2019-11-27 19:33:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,13 @@ const Discord = require('discord.js');
 const discord = new Discord.Client();
  
 console.log("TAR7EB");
-      if (member.guild.id === "635898451640582184") {
+ 
 
 client.on("guildMemberAdd", member => {
+     if (member.guild.id === "635898451640582184") {
 message.channel.send(` have a good moment with **kova** ‘ ${member} `)
 
 }
-})           
+})      
               
 client.login(process.env.BOT_TOKEN);
