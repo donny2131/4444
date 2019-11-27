1c68e153435c51110179a0d79f8dcf023a9246e0
donny2131 <55747762+donny2131@users.noreply.github.com>
2019-11-27 20:18:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ console.log("TAR7EB");
 
 client.on("guildMemberAdd", member => {
      if (member.guild.id === "635898451640582184") {
-  return channel.send(`  have a good moment with **kova** ‘ ${member} `)
-
+  return channel.send(` have a good moment with **kova** ‘ ${member} `)
+var channel =member.guild.channels.find('name', 'sayhi')
 
 }
-})     
+})   
               
 client.login(process.env.BOT_TOKEN);
